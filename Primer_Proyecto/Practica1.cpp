0ca2df264de23040c27a870a47debab196da272e
diegoca04 <diegocasado2004@gmail.com>
2025-10-02 18:13:25 +0200

    creación archivo practica 1

---
+++ Primer_Proyecto/Practica1.cpp
@@ -0,0 +1,24 @@
+#define PROYECTO "Práctica 1"
+
+#include <iostream>	
+#include <codebase.h>
+						
+
+int main(int argc, char** argv)
+{
+	// Inicializaciones
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(50, 50);
+
+	// Crear ventana
+	glutCreateWindow(PROYECTO);
+
+	// Registrar callbacks
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+
+	// Bucle de atencion a eventos
+	glutMainLoop();
+}
