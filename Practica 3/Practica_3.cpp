9e8f29fe8832e84832b60c5fc83710ac8c7cdb3b
diegoca04 <diegocasado2004@gmail.com>
2025-11-12 13:17:33 +0100

    .

--- Practica 3/Practica_3.cpp
+++ Practica 3/Practica_3.cpp
@@ -5,7 +5,7 @@
 #include <codebase.h>
 #include <vector>
 
-GLuint anilloMenor, anilloMayor;
+GLuint engranaje1, engranaje2;
 
 std::vector<cb::Vec3> puntosCircunferencia(int numeroPuntos, float radio, float fase = 0, float z = 0.0f)
 {
@@ -26,17 +26,25 @@ std::vector<cb::Vec3> puntosCircunferencia(int numeroPuntos, float radio, float 
 	return res;
 }
 
-GLuint anillo(float diamExt, float diamInt, float grosor, float res = 20)
+GLuint engranajeExterior(float diametroPrimitivo, float alturaDiente, float
+	diametroEje, float grosor, unsigned int numeroDientes)
 {
 	GLuint id = glGenLists(1);
 	glNewList(id, GL_COMPILE);
-	//glBegin(GL_POINTS);
 
-	std::vector<cb::Vec3> verticesInt = puntosCircunferencia(res, diamInt / 2.0f);
-	std::vector<cb::Vec3> verticesExt = puntosCircunferencia(res, diamExt / 2.0f);
+	float diamExt = diametroPrimitivo - alturaDiente / 2;
 
-	std::vector<cb::Vec3> verticesInt2 = puntosCircunferencia(res, diamInt / 2.0f, 0, grosor);
-	std::vector<cb::Vec3> verticesExt2 = puntosCircunferencia(res, diamExt / 2.0f, 0, grosor);
+	std::vector<cb::Vec3> verticesInt = puntosCircunferencia(numeroDientes, diametroEje / 2.0f);
+	std::vector<cb::Vec3> verticesExt = puntosCircunferencia(numeroDientes, diamExt / 2.0f);
+
+	std::vector<cb::Vec3> verticesInt2 = puntosCircunferencia(numeroDientes, diametroEje / 2.0f, 0, grosor);
+	std::vector<cb::Vec3> verticesExt2 = puntosCircunferencia(numeroDientes, diamExt / 2.0f, 0, grosor);
+
+	float n = 360 / (2 * numeroDientes);
+
+	std::vector<cb::Vec3> verticesDientes = puntosCircunferencia(numeroDientes, (diamExt + alturaDiente) / 2.0f, n);
+	std::vector<cb::Vec3> verticesDientes2 = puntosCircunferencia(numeroDientes, 
+		(diamExt + alturaDiente) / 2.0f, n, grosor);
 
 	int N = verticesInt.size();
 
@@ -45,6 +53,191 @@ GLuint anillo(float diamExt, float diamInt, float grosor, float res = 20)
 	vertices.insert(vertices.end(), verticesExt.begin(), verticesExt.end());
 	vertices.insert(vertices.end(), verticesInt2.begin(), verticesInt2.end());
 	vertices.insert(vertices.end(), verticesExt2.begin(), verticesExt2.end());
+	vertices.insert(vertices.end(), verticesDientes.begin(), verticesDientes.end());
+	vertices.insert(vertices.end(), verticesDientes2.begin(), verticesDientes2.end());
+
+	std::vector<GLuint> indices;
+	for (int i = 0; i < N; i++) {
+		int next = (i + 1) % N;
+		indices.push_back(i);
+		indices.push_back(next);
+		indices.push_back(next + N);
+		indices.push_back(i + N);
+	}
+	for (int i = 2 * N; i < 3 * N; i++) {
+		int next = (i + 1 - 2 * N) % N + 2 * N;
+		indices.push_back(i);
+		indices.push_back(next);
+		indices.push_back(next + N);
+		indices.push_back(i + N);
+	}
+	for (int i = 0; i < N; i++) {
+		int next = (i + 1) % N;
+		indices.push_back(i);
+		indices.push_back(next);
+		indices.push_back(next + 2 * N);
+		indices.push_back(i + 2 * N);
+	}
+	for (int i = 0; i < N; i++) {
+		indices.push_back(i + N);
+		indices.push_back(i + 4 * N);
+		indices.push_back(i + 5 * N);
+		indices.push_back(i + 3 * N);
+	}
+	for (int i = 0; i < N; i++) {
+		int next = (i + 1) % N;
+		indices.push_back(i + 4 * N);
+		indices.push_back(next + N);
+		indices.push_back(next + 3 * N);
+		indices.push_back(i + 5 * N);
+	}
+
+	std::vector<GLuint> indicesDientes;
+	for (int i = N; i < 2 * N; i++) {
+		int next = (i + 1 - N) % N + N;
+		indicesDientes.push_back(i);
+		indicesDientes.push_back(next);
+		indicesDientes.push_back(i + 3 * N);
+	}
+	for (int i = 3 * N; i < 4 * N; i++) {
+		int next = (i + 1 - 3 * N) % N + 3 * N;
+		indicesDientes.push_back(i);
+		indicesDientes.push_back(next);
+		indicesDientes.push_back(i + 2 * N);
+	}
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(3, GL_FLOAT, 0, vertices.data());
+	glDrawElements(GL_QUADS, indices.size(), GL_UNSIGNED_INT, indices.data());
+	glDrawElements(GL_TRIANGLES, indicesDientes.size(), GL_UNSIGNED_INT, indicesDientes.data());
+	glDisableClientState(GL_VERTEX_ARRAY);
+
+	glEnd();
+
+	glEndList();
+	return id;
+}
+
+GLuint engranajeInterior(float diametroPrimitivo, float alturaDiente, float
+	diametroExterior, float grosor, unsigned int numeroDientes)
+{
+	GLuint id = glGenLists(1);
+	glNewList(id, GL_COMPILE);
+
+	float diamInt = diametroPrimitivo - alturaDiente / 2;
+
+	std::vector<cb::Vec3> verticesInt = puntosCircunferencia(numeroDientes, diamInt / 2.0f);
+	std::vector<cb::Vec3> verticesExt = puntosCircunferencia(numeroDientes, diametroExterior / 2.0f);
+
+	std::vector<cb::Vec3> verticesInt2 = puntosCircunferencia(numeroDientes, diamInt / 2.0f, 0, grosor);
+	std::vector<cb::Vec3> verticesExt2 = puntosCircunferencia(numeroDientes, diametroExterior / 2.0f, 0, grosor);
+
+	float n = 360 / (2 * numeroDientes);
+
+	std::vector<cb::Vec3> verticesDientes = puntosCircunferencia(numeroDientes, (diamInt - alturaDiente) / 2.0f, n);
+	std::vector<cb::Vec3> verticesDientes2 = puntosCircunferencia(numeroDientes, 
+		(diamInt - alturaDiente) / 2.0f, n, grosor);
+
+	int N = verticesInt.size();
+
+	std::vector<cb::Vec3> vertices;
+	vertices.insert(vertices.end(), verticesInt.begin(), verticesInt.end());
+	vertices.insert(vertices.end(), verticesExt.begin(), verticesExt.end());
+	vertices.insert(vertices.end(), verticesInt2.begin(), verticesInt2.end());
+	vertices.insert(vertices.end(), verticesExt2.begin(), verticesExt2.end());
+	vertices.insert(vertices.end(), verticesDientes.begin(), verticesDientes.end());
+	vertices.insert(vertices.end(), verticesDientes2.begin(), verticesDientes2.end());
+
+	std::vector<GLuint> indices;
+	for (int i = 0; i < N; i++) {
+		int next = (i + 1) % N;
+		indices.push_back(i);
+		indices.push_back(next);
+		indices.push_back(next + N);
+		indices.push_back(i + N);
+	}
+	for (int i = 2 * N; i < 3 * N; i++) {
+		int next = (i + 1 - 2 * N) % N + 2 * N;
+		indices.push_back(i);
+		indices.push_back(next);
+		indices.push_back(next + N);
+		indices.push_back(i + N);
+	}
+	for (int i = N; i < 2 * N; i++) {
+		int next = (i + 1 - N) % N + N;
+		indices.push_back(i);
+		indices.push_back(next);
+		indices.push_back(next + 2 * N);
+		indices.push_back(i + 2 * N);
+	}
+	for (int i = 0; i < N; i++) {
+		indices.push_back(i);
+		indices.push_back(i + 4 * N);
+		indices.push_back(i + 5 * N);
+		indices.push_back(i + 2 * N);
+	}
+	for (int i = 0; i < N; i++) {
+		int next = (i + 1) % N;
+		indices.push_back(i + 4 * N);
+		indices.push_back(next);
+		indices.push_back(next + 2 * N);
+		indices.push_back(i + 5 * N);
+	}
+
+	std::vector<GLuint> indicesDientes;
+	for (int i = 0; i < N; i++) {
+		int next = (i + 1) % N;
+		indicesDientes.push_back(i);
+		indicesDientes.push_back(next);
+		indicesDientes.push_back(i + 3 * N);
+	}
+	for (int i = 2 * N; i < 3 * N; i++) {
+		int next = (i + 1 - 2 * N) % N + 2 * N;
+		indicesDientes.push_back(i);
+		indicesDientes.push_back(next);
+		indicesDientes.push_back(i + 2 * N);
+	}
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(3, GL_FLOAT, 0, vertices.data());
+	glDrawElements(GL_QUADS, indices.size(), GL_UNSIGNED_INT, indices.data());
+	glDrawElements(GL_TRIANGLES, indicesDientes.size(), GL_UNSIGNED_INT, indicesDientes.data());
+	glDisableClientState(GL_VERTEX_ARRAY);
+
+	glEnd();
+
+	glEndList();
+	return id;
+}
+
+GLuint engranajeCara(float diametroPrimitivo, float alturaDiente, float
+	diametroInterior, float grosor, unsigned int numeroDientes) 
+{
+	GLuint id = glGenLists(1);
+	glNewList(id, GL_COMPILE);
+
+	std::vector<cb::Vec3> verticesInt = puntosCircunferencia(numeroDientes, diametroInterior / 2.0f);
+	std::vector<cb::Vec3> verticesExt = puntosCircunferencia(numeroDientes, diametroPrimitivo / 2.0f);
+
+	std::vector<cb::Vec3> verticesInt2 = puntosCircunferencia(numeroDientes, diametroInterior / 2.0f, 0, grosor);
+	std::vector<cb::Vec3> verticesExt2 = puntosCircunferencia(numeroDientes, diametroPrimitivo / 2.0f, 0, grosor);
+
+	float n = 360 / (2 * numeroDientes);
+
+	std::vector<cb::Vec3> verticesDientesInt = puntosCircunferencia(numeroDientes, 
+		(diametroInterior) / 2.0f, n, 0 - alturaDiente);
+	std::vector<cb::Vec3> verticesDientesExt = puntosCircunferencia(numeroDientes, 
+		(diametroPrimitivo) / 2.0f, n, 0 - alturaDiente);
+
+	int N = verticesInt.size();
+
+	std::vector<cb::Vec3> vertices;
+	vertices.insert(vertices.end(), verticesInt.begin(), verticesInt.end());
+	vertices.insert(vertices.end(), verticesExt.begin(), verticesExt.end());
+	vertices.insert(vertices.end(), verticesInt2.begin(), verticesInt2.end());
+	vertices.insert(vertices.end(), verticesExt2.begin(), verticesExt2.end());
+	vertices.insert(vertices.end(), verticesDientesInt.begin(), verticesDientesInt.end());
+	vertices.insert(vertices.end(), verticesDientesExt.begin(), verticesDientesExt.end());
 
 	std::vector<GLuint> indices;
 	for (int i = 0; i < N; i++) {
@@ -75,31 +268,52 @@ GLuint anillo(float diamExt, float diamInt, float grosor, float res = 20)
 		indices.push_back(next + 2 * N);
 		indices.push_back(i + 2 * N);
 	}
+	//Cambiar estos dos bucles
+	for (int i = 0; i < N; i++) {
+		indices.push_back(i + N);
+		indices.push_back(i + 4 * N);
+		indices.push_back(i + 5 * N);
+		indices.push_back(i + 3 * N);
+	}
+	for (int i = 0; i < N; i++) {
+		int next = (i + 1) % N;
+		indices.push_back(i + 4 * N);
+		indices.push_back(next + N);
+		indices.push_back(next + 3 * N);
+		indices.push_back(i + 5 * N);
+	}
 
-	/*glBegin(GL_LINE_LOOP);
-	for (auto& p : verticesInt)
-		glVertex3fv(p);
-	glEnd();
-
-	glBegin(GL_LINE_LOOP);
-	for (auto& p : verticesExt)
-		glVertex3fv(p);
-	glEnd();*/
+	std::vector<GLuint> indicesDientes;
+	//Cambiar estos dos también
+	for (int i = N; i < 2 * N; i++) {
+		int next = (i + 1 - N) % N + N;
+		indicesDientes.push_back(i);
+		indicesDientes.push_back(next);
+		indicesDientes.push_back(i + 3 * N);
+	}
+	for (int i = 3 * N; i < 4 * N; i++) {
+		int next = (i + 1 - 3 * N) % N + 3 * N;
+		indicesDientes.push_back(i);
+		indicesDientes.push_back(next);
+		indicesDientes.push_back(i + 2 * N);
+	}
 
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glVertexPointer(3, GL_FLOAT, 0, vertices.data());
 	glDrawElements(GL_QUADS, indices.size(), GL_UNSIGNED_INT, indices.data());
+	glDrawElements(GL_TRIANGLES, indicesDientes.size(), GL_UNSIGNED_INT, indicesDientes.data());
 	glDisableClientState(GL_VERTEX_ARRAY);
 
-	//glEnd();
+	glEnd();
+
 	glEndList();
 	return id;
 }
 
 void init()
 {
-	anilloMenor = anillo(1.2, 0.6, 0.2, 30);
-	anilloMayor = anillo(2, 1.6, 0.2, 40);
+	engranaje1 = engranajeExterior(1.2, 0.2, 0.8, 0.2, 30);
+	engranaje2 = engranajeInterior(1.6, 0.2, 1.8, 0.2, 30);
 }
 
 void display()
@@ -110,22 +324,23 @@ void display()
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	gluLookAt(0, 0, 0, -0.7, 0.3, -0.7, 0, 1, 0);
+	gluLookAt(0, 0, 0, 0, 0, -1, 0, 1, 0);
+	//gluLookAt(0, 0, 0, -0.7, 0.3, -0.7, 0, 1, 0);
+	//gluLookAt(3, 2, 3, 0, 0, 0, 0, 1, 0);
 
 	cb::ejes();
 
 	glColor3f(0.0, 0.0, 0.0);
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-	glCallList(anilloMenor);
-	glCallList(anilloMayor);
+	glCallList(engranaje1);
+	glCallList(engranaje2);
 
 	glColor3f(1, 0, 0);
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	glCallList(anilloMenor);
-	glCallList(anilloMayor);
+	glCallList(engranaje1);
+	glCallList(engranaje2);
 
 	glutSwapBuffers();
-	//glFlush();
 }
 
 void reshape(GLint w, GLint h)
